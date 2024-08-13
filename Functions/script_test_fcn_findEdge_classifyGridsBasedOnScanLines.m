7d220817211e1a4f29849a5b4aba2b024bf371cb
AlexGonch321 <goncharovaleksandr2002@gmail.com>
2024-08-13 12:02:38 -0400

    FEAT: Functionalized STEP 6_2 which classifies the grids based on scan lines. Script is not finished and uses actual data, needs basic examples.

---
+++ Functions/script_test_fcn_findEdge_classifyGridsBasedOnScanLines.m
@@ -0,0 +1,9 @@
+% FORMAT:
+%
+%      fcn_findEdge_classifyGridsBasedOnScanLines(grids_greater_than_zero_points...
+%      ,total_scan_lines_in_each_grid_with_more_than_zero_points,gridCenters, (format_1),(format_2),
+%      (fig_num))
+%
+
+
+fcn_findEdge_classifyGridsBasedOnScanLines(grids_greater_than_zero_points,total_scan_lines_in_each_grid_with_more_than_zero_points,gridCenters,[],[],555)
