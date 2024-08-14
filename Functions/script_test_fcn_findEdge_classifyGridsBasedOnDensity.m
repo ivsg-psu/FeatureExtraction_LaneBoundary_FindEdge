97e3b36071442d195028d573e544bb8f3deec337
AlexGonch321 <goncharovaleksandr2002@gmail.com>
2024-08-14 09:06:08 -0400

    FEAT: Functionalized STEP 5 Part 1 called determineGridPointDensity. The script attached is incomplete and needs to have additional cases added to it.

--- Functions/script_test_fcn_findEdge_classifyGridsBasedOnDensity.m
+++ Functions/script_test_fcn_findEdge_classifyGridsBasedOnDensity.m
@@ -1,1 +1,5 @@
-fcn_findEdge_classifyGridsBasedOnDensity(grids_greater_than_zero_points,total_N_points_in_each_grid,point_density,gridCenters,[],[],23)
+%% SCRIPT OF JUST RUNNING THE FUNCTION (NEEDS WORK)
+
+fig_num=2223;
+
+fcn_findEdge_classifyGridsBasedOnDensity(grids_greater_than_zero_points,total_N_points_in_each_grid,point_density,gridCenters,[],[],fig_num);
